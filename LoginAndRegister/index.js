df19f8e801a06a5783f983d332e1fe9382a61400
thanhthtung317 <nguyenththanhtung317@gmail.com>
2021-11-18 20:49:35 +0700

    delete readme file and start to add logic to login feature

--- LoginAndRegister/index.js
+++ LoginAndRegister/index.js
@@ -1,12 +1,21 @@
-import axios from "axios";
-let username = document.querySelector("#username");
-let password = document.querySelector("#password");
+let usernameDOM = document.querySelector("#username");
+let passwordDOM = document.querySelector("#password");
 const form = document.querySelector('form');
+let username = '';
+let password = '';
+
+usernameDOM.oninput = (e) => {
+  username = e.target.value;
+};
+
+passwordDOM.oninput = (e) => {
+  password = e.target.value;
+};
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     location.replace(
-     "../mainApp/index.html"
+      "../mainApp/index.html"
     );
 })
 
@@ -32,3 +41,4 @@ async function isLoggedIn() {
   const token = store.get("token");
   if (!token) return false;
 }
+
